7f3d5053c3dccfa58fae7752680f015587beef26
bodymindarts <justin@galoy.io>
2023-08-03 11:21:15 +0200

    chore: bump briaadmin provider in example

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     briaadmin = {
       source  = "galoymoney/briaadmin"
-      version = "0.0.6"
+      version = "0.0.7"
     }
     bria = {
       source  = "galoymoney/bria"
